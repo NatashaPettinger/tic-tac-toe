ae87f04c52e6dfd1a7ca349f131adc72de52288c
NatashaPettinger <natasha.pettinger@gmail.com>
2022-04-26 10:12:38 -0600

    fixed some of the functionality issues.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 This is an API for bartenders who just need a little inspiration. It returns ingredients & instructions for different cocktails, and searched by ingredient. 
 
 Data from https://www.thecocktaildb.com/api.php
+
+Hosted at https://bartenders-choice.netlify.app/
--- assets/js/api.js
+++ assets/js/api.js
@@ -1,18 +1,29 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
-document.getElementById('searchButton').addEventListener('click', newPage)
+function addEvents() {
+  document.querySelector('#searchButton').addEventListener('click', searchResults)
+  document.getElementById('search').addEventListener('keypress', function(event) {
+  if (event.key === "Enter"){
+    event.preventDefault();
+    document.getElementById('searchButton').click();
+  }})
+}
+addEvents();
 
+/* 
 function newPage() {
   let drink = `i=${document.getElementById('search').value}`;
   window.open('results.html', '_self');
   searchResults(drink)
 }
-
-function searchResults(drink) {
-
+ */
+function searchResults() {
+  let drink = `i=${document.getElementById('search').value}`;
   //hide drink recipe, if shown.
-  if (document.querySelector('.wide').classList === 'wide') {
+  /* if (document.querySelector('.wide').classList === 'wide') {
     document.querySelector('.wide').classList += 'hidden';
-  }
+  } */
+  document.querySelector('.wide').classList.add('hidden');
+  document.querySelector('.searchSection').classList.remove('hidden');
   //hide header to make more room for search results.
   document.getElementById('header').classList = 'hidden';
 
@@ -31,7 +42,9 @@ function searchResults(drink) {
         idNumbers.push(x.idDrink);});
       
       //add choose for me button to DOM
-      document.getElementById('signup-form').innerHTML += `<input id="chooseRandom" class="clink" type="button" value="Choose for me!" />`
+      if (!document.getElementById('chooseRandom')) {
+        document.getElementById('search-form').innerHTML += `<input id="chooseRandom" class="clink" type="button" value="Choose for me!" />`
+      }
       
       //clear search results string & add links to cocktail recipes.
       let searchResults = '';
@@ -39,23 +52,28 @@ function searchResults(drink) {
         searchResults += `<li><a  id="i${i}" class="clink">${x}</a></li>`;
       })
       document.getElementById('searchResults').innerHTML = searchResults;
+
+      //add event listeners
       Array.from(document.querySelectorAll('.clink')).forEach(d => d.addEventListener('click', findDrink))
+      addEvents()
 
       let index
       function findDrink(click) {
         index = click.target.id;
         if (index === "chooseRandom") {
           index = Math.floor(Math.random() * data.drinks.length);
-        } else index = index.slice(1);
-        getDrink(idNumbers[index]);
+        } else {
+          index = index.slice(1);
+        }
+          getDrink(idNumbers[index]);
       }
-
       })
     .catch(err => {
         console.log(`error ${err}`)
     });
 
 }
+
 
 function getDrink(j){
   fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${j}`)
@@ -68,6 +86,7 @@ function getDrink(j){
         document.querySelector('h2').innerText = cocktail.strDrink
         document.querySelector('img').src = cocktail.strDrinkThumb
         document.querySelector('.wide').classList.remove('hidden')
+        document.querySelector('.searchSection').classList.add('hidden');
         //parse ingredients
         let ing = [];
         for (let i = 1; i <= 15; i++) {
@@ -99,12 +118,10 @@ function getDrink(j){
         inst.forEach(y => {
           document.getElementById('instructionList').innerHTML += `<li>${y}.</li>`
       })
+
     })
     .catch(err => {
         console.log(`error ${err}`)
     });
 
 }
-
-//add pick random functionality
-//make gallery of drinks w/ their names
